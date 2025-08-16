ac1ab69320d9c025e0739b87cfb20f9ccbb345af
Vitalii Bokov <mozilla95@list.ru>
2025-08-16 14:29:23 +0300

    added postman collection and changed README.md

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ PID=$!
 # Ждем, когда брокер станет готов к управлению ACL
 until kafka-broker-api-versions --bootstrap-server kafka-0:9093 --command-config /etc/kafka/properties/client.properties; do
   echo "Waiting for Kafka to be ready..."
-  sleep 2
+  sleep 1
 done
 
 # Применяем ACL в нужной последовательности
@@ -16,5 +16,5 @@ kafka-acls --bootstrap-server kafka-0:9093 --add --allow-principal User:'consume
 kafka-acls --bootstrap-server kafka-0:9093 --add --allow-principal User:'consumer1' --operation DESCRIBE --group kafka-security-message --command-config /etc/kafka/properties/client.properties
 kafka-acls --bootstrap-server kafka-0:9093 --add --allow-principal User:'consumer1' --operation READ --topic topic-1 --command-config /etc/kafka/properties/client.properties
 
-## Ждем завершения фонового процесса, если нужно
+## Ждем завершения фонового процесса
 wait $PID
